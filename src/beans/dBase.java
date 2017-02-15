b2fcddd241feab348976e36603eb441db0184d30
Randive <priyasanjayrandive@gmail.com>
2017-02-15 17:29:47 +0530

    Update dBase.java

--- src/beans/dBase.java
+++ src/beans/dBase.java
@@ -31,10 +31,11 @@ public class dBase {
 		String PASS = System.getenv("PASS");
 		System.out.println("Printing Environment Variables");
 		//System.out.println("tempVar"+tempVar);
-		System.out.println("url"+URL);
-		System.out.println("user"+USER);
-		System.out.println("password"+PASS);
+		System.out.println("url=="+URL);
+		System.out.println("user=="+USER);
+		System.out.println("password=="+PASS);
                 Connection connection  = DriverManager.getConnection(URL,USER,PASS);
+		System.out.println("ConnectionObject"+connection);
 		//connection = DriverManager.getConnection(
 				//"jdbc:mysql://35.166.130.227:3306/book_my_doc", "root", "root");
 		statement = connection.createStatement();
