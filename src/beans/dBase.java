6730bec5d11666a576eebe55fc1f971430f13103
Randive <priyasanjayrandive@gmail.com>
2017-02-24 11:24:02 +0530

    adding new dBase file

--- src/beans/dBase.java
+++ src/beans/dBase.java
@@ -1,10 +1,17 @@
+
 package beans;
 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Map;
+import java.util.Properties;
 
 
 public class dBase {
@@ -12,33 +19,64 @@ public class dBase {
 	private Connection connection;
 	private Statement statement;
 	private ResultSet results = null;
+	
 	@SuppressWarnings("unused")
 	private String query;
 
-	public void createConn() throws ClassNotFoundException, SQLException {
+	public void createConn() throws ClassNotFoundException,SQLException, IOException {
+		//System.getProperty("cisco.life");
+		//String testvar=System.getProperty("cisco.life");
+		//String testvar=System.getenv("cisco.life");
+		//System.out.println("Cisco var "+testvar);
+		
+/*		Map<Object, Object> env = System.getProperties();
+        for (Object envName : env.keySet()) {
+            System.out.format("%s=%s%n",
+                              (String) envName,
+                              env.get((String) envName));
+        }*/
+				
 		Class.forName("com.mysql.jdbc.Driver");
-		connection = DriverManager.getConnection(
-				"jdbc:mysql://52.37.29.122:3306/book_my_doc", "root", "root");
-		//String URL = ${URL};
-                //String USER = ${USER};
-                //String PASS = ${PASS};
-		//String tempVar = System.getenv("CISCO");
-		//String URL = System.getProperty("URL");
+		
+		System.out.println("***Adding NEw *******");
+		Properties props = new Properties();
+		FileInputStream in = new FileInputStream("C:\\dcloud\\db.properties");
+		props.load(in);
+		in.close();
+
+		String driver = props.getProperty("jdbc.driver");
+		if (driver != null) {
+		    Class.forName(driver) ;
+		}
+
+		String url = props.getProperty("jdbc.url");
+		String username = props.getProperty("jdbc.username");
+		String password = props.getProperty("jdbc.password");
+
+		Connection connection = DriverManager.getConnection(url, username, password);
+		System.out.println("***Ending NEw *******");
+		/*//String URL = System.getProperty("URL");
 		//String USER = System.getProperty("USER");
 		//String PASS = System.getProperty("PASS");
-		//String URL = System.getenv("URL");
-		//String USER = System.getenv("USER");
-		//String PASS = System.getenv("PASS");
-		//System.out.println("Printing Environment Variables");
-		//System.out.println("tempVar"+tempVar);
-		//System.out.println("url=="+URL);
-		//System.out.println("user=="+USER);
-		//System.out.println("password=="+PASS);
-                //Connection connection  = DriverManager.getConnection(URL,USER,PASS);
-		System.out.println("ConnectionObject"+connection);
+		String URL = "jdbc:mysql://localhost:3306/book_my_doc";
+		String USER = "root";
+		String PASS = "redhat";
+		System.out.println("Ne URL == "+URL);
+		System.out.println("USER == "+USER);
+		System.out.println("PASS == "+PASS);
+                Connection connection  = DriverManager.getConnection(URL,USER,PASS);
+//connection = DriverManager.getConnection(*/
+		
+		
 		//connection = DriverManager.getConnection(
-				//"jdbc:mysql://35.166.130.227:3306/book_my_doc", "root", "root");
-		statement = connection.createStatement();
+				//"jdbc:mysql://doctordb@zenims.com:3306/book_my_doc", "root", "root");
+		//System.out.println(DatabasePassword);
+		
+		System.out.println("ConnectionObject"+connection);
+
+        statement = connection.createStatement();
+		
+		
 	}
 
 	public ResultSet executeQuery(String q) throws SQLException {
@@ -72,9 +110,6 @@ public class dBase {
 	}
 
 	public void closeConn() throws SQLException {
-		//statement.close();
-		//connection.close();
-	//}
 		statement.close();
 		//connection.close();
 		if (connection != null) {
@@ -82,4 +117,5 @@ public class dBase {
 		}	
 		}
 
+	
 }
