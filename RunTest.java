f02180563ca20826c4ad63e8154513add898fc2a
BalaNageswar <baluchb4u@gmail.com>
2019-05-28 23:39:38 -0700

    Changed -HTML format
    Changed the format for Cucumber reports in test runner file

--- RunTest.java
+++ RunTest.java
@@ -1,8 +1,15 @@
-package junitcucumber;
- 
-import org.junit.runner.RunWith;
-import cucumber.api.junit.Cucumber;
- 
-@RunWith(Cucumber.class)
-public class UserTest {
-}
+package CucumberReport; 
+
+import org.junit.runner.RunWith; 
+import cucumber.junit.Cucumber; 
+
+@RunWith(Cucumber.class) 
+@Cucumber.Options( 
+   format = {"pretty", "html:target/Destination"} ) 
+	
+//Specifying pretty as a format option ensure that HTML report will be generated. 
+//When we specify html:target/Destination - It will generate the HTML report 
+
+inside the Destination folder, in the target folder of the maven project. 
+
+public class runTest { }
