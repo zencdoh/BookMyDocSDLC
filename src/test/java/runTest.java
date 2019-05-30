13d4c923560754ccc41ee0c4ff27272311f908ee
zencdoh <adminhybridit@imsrndlabs.com>
2019-05-30 12:02:11 +0530

    changed code to syntax
    bala changed syntax

--- src/test/java/runTest.java
+++ src/test/java/runTest.java
@@ -1,11 +1,10 @@
 package CucumberReport; 
-
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-//@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(
     format = (features = {/src/test/java/features/}, format = {" json :target/cucumber.json"}, glue = "steps")
     public class CucumberReport extends AbstractTestNGCucumberTests
