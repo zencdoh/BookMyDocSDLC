1dcf3da2cd0fac03852f3e29ad62e71dd8ffcf53
zencdoh <adminhybridit@imsrndlabs.com>
2019-05-30 08:15:26 +0530

    Working on format
    Bala-working on format

--- RunTest.java
+++ RunTest.java
@@ -1,33 +1,18 @@
 
 
-//import org.junit.runner.RunWith; 
-//import cucumber.junit.Cucumber; 
-
-//@RunWith(Cucumber.class) 
-//@CucumberOptions (format = {"pretty", "html:target/Destination"} )
-//@Cucumber.Options( 
- //  format = {"pretty", "html:target/Destination"} ) 
-	
-//Specifying pretty as a format option ensure that HTML report will be generated. 
-//When we specify html:target/Destination - It will generate the HTML report 
-
-//inside the Destination folder, in the target folder of the maven project. 
-
-//public class runTest { }
 
 package CucumberReport; 
 
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
-    format = {"pretty", "html:target/Destination"},
-    features={"BookMyDocSDLC/User.feature"}
-	
-	
-    )
-public class User{
+    format = (features = {/src/test/java/features/}, format = { json :target/cucumner.json"}, glue = "steps")
+    public class CucumberReport extends AbstractTestNGCucumberTests
+    {
+
 }
 
