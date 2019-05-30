da37c0a28f867aad2864e4f340067e0d47d8c869
zencdoh <adminhybridit@imsrndlabs.com>
2019-05-30 12:28:11 +0530

    compile isue
    bala-updated compilation issue

--- src/test/java/runTest.java
+++ src/test/java/runTest.java
@@ -16,11 +16,20 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
  
-@RunWith(Cucumber.class)
- @CucumberOptions(
-    format = (features = {/src/test/java/features/}, format = {" json :target/cucumber.json"}, glue = "steps")
-)
+/*@RunWith(Cucumber.class)
+ @CucumberOptions(format = (features = {/src/test/java/features/}, format = {" json :target/cucumber.json"}, glue = "steps")
+)*/
  
 public class CucumberReport {
  
+@RunWith(Cucumber.class)
+@CucumberOptions(
+dryRun=true,
+strict=true,
+features={"/src/test/java/features/user.feature"},
+glue={"stepDefinition"},
+tags={"@automated"},
+plugin={"json:src/test/cucumber.json"}
+)
+public class CucumberReport {
 }
